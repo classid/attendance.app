dd9fc1e386e5a6995fcd3768ae6ab457afc2dcfd
Muhammad Yuqi <muhammad.yuqi@gmail.com>
2020-01-03 00:12:50 +0700

    updt

--- config/cid/app.php
+++ config/cid/app.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'name' => 'Al-Hudhur',
-    'version' => '1.0',
+    'version' => '1.1',
 
     'attendance_sync' => env('ATTENDANCE_SYNC', 'http://smamda.class.id/api/academic/presence/attendance'),
 ];
